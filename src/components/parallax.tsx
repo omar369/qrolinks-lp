298271ad72e62193d0fb828defb01e47274705eb
omar369 <34948096+omar369@users.noreply.github.com>
2024-05-28 10:34:12 -0600

    ajuste de fondo parallax

--- src/components/parallax.tsx
+++ src/components/parallax.tsx
@@ -8,7 +8,12 @@ const Parallax: React.FC<ParallaxProps> = ({ children }) => {
   return (
     <div
       className="bg-fixed bg-center bg-cover h-screen"
-      style={{ backgroundImage: "url('/images/portada1.jpg')" }}
+      style={{
+        backgroundImage: "url('/images/portada1.jpg')",
+        backgroundSize: 'cover', // Ajusta el tamaño de la imagen para cubrir todo el contenedor
+        backgroundRepeat: 'no-repeat', // Evita que la imagen se repita
+        backgroundPosition: 'center', // Centra la imagen en el contenedor
+      }}
     >
       {children}
     </div>
