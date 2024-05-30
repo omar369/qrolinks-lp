8ab5a061c37898d0c4658eeffe0ab2e1ad69c94f
omar369 <34948096+omar369@users.noreply.github.com>
2024-05-29 20:15:27 -0600

    error al enviar correo contacto

--- src/app/pages/api/sendEmail.ts
+++ src/app/pages/api/sendEmail.ts
@@ -2,33 +2,33 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    const { name, email, reason, message } = req.body;
-
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: process.env.GMAIL_USER,
-      to: process.env.GMAIL_USER, // Puedes cambiar esto a otro email o lista de emails
-      subject: `Mensaje de contacto: ${reason}`,
-      text: `Nombre: ${name}\nEmail: ${email}\nMensaje:\n${message}`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return res.status(500).json({ error: error.message });
-      } else {
-        return res.status(200).json({ message: 'Email enviado con éxito' });
-      }
-    });
-  } else {
+  if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
+  const { name, email, reason, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.GMAIL_USER,
+    to: process.env.GMAIL_USER, // Cambia esto a otro email si es necesario
+    subject: `Mensaje de contacto: ${reason}`,
+    text: `Nombre: ${name}\nEmail: ${email}\nMensaje:\n${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    } else {
+      return res.status(200).json({ message: 'Email enviado con éxito' });
+    }
+  });
 }
